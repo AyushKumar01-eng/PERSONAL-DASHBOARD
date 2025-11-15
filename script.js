6dd8cab6f83bde73276ae5df830fd1627dbf7cc6
ayush <akslapy@gmail.com>
2025-11-15 09:31:01 +0530

    build notes pad functional

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ mode.addEventListener('change', () => {
         document.body.classList.remove('darkmode')
     }
 })
+
+
+//NOTES PAD
+
+function render() {
+    const stored = localStorage.getItem('note');
+    if (stored) {
+        noteContainer.innerHTML = `
+      <h1>TODAY'S NOTE:</h1>
+      <h3>${stored}</h3>
+      <button class="dlt">Delete Note</button>
+    `
+        const delBtn = noteContainer.querySelector('.dlt')
+        delBtn.addEventListener('click', () => {
+            localStorage.removeItem('note')
+            render()
+        })
+    } else {
+        noteContainer.innerHTML = `
+      <input id="noteadders" type="text" placeholder="WRITE A NOTE">
+      <button class="notebtn">ADD NOTE</button>
+    `
+        const input = document.getElementById('noteadders')
+        const addBtn = noteContainer.querySelector('.notebtn')
+console.log(input,addBtn);
+    }
+}
+
+render()
